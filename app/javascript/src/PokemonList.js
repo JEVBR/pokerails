66fb42d00b676ede97db937c8d0e699d829ee346
Jeroen Haisma <J_Haisma@hotmail.com>
2019-09-18 10:02:59 -0300

    added useEffect preparing for actioncable

--- app/javascript/src/PokemonList.js
+++ app/javascript/src/PokemonList.js
@@ -7,8 +7,20 @@ export default function PokemonList({ pokemon }) {
   const [test, setTest] = useState([]);
 
   useEffect(() => {
-    //subsctibe to cable here
+    //subsctibe to cable here liki ths ex:
+    // App.chatroom = App.cable.subscriptions.create({
+    //   channel: "ChatroomChannel",
+    //   id: this.state.chatroomId
+    //   },
+    //   {
+    //     received: function(data) {
+    //       this.updateMessages(JSON.parse(data.message))
+    //     }.bind(this)
+    //   });
 
+    //https://reactjs.org/docs/hooks-intro.html
+    //https://github.com/learnetto/reactchat/blob/master/client/app/bundles/Chatroom/components/Chatroom.jsx
+    //return () => { // unsubscribe here}
   },[]);
 
   useEffect(() => {
